81438efa506225afb00004e63c80c3a9a4a72b90
RAIHAN <raihanbadsha363@gmail.com>
2021-04-07 20:49:53 +0600

    Airbnb-clone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+import SearchPage from './components/SearchPage';
+import Footer from './components/Footer';
+
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './components/firebase.config';
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+    <Router>
+      <Header />
+      
+      <Switch>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      
+      <Footer />
+    </Router>
+  </div>
   );
 }
 
---
+++ src/components/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCspYB59vf5CVcGvcX7zqqc6zhaVMBx23o",
+    authDomain: "airbnb-clone-9c9f2.firebaseapp.com",
+    projectId: "airbnb-clone-9c9f2",
+    storageBucket: "airbnb-clone-9c9f2.appspot.com",
+    messagingSenderId: "1083714185853",
+    appId: "1:1083714185853:web:662f966ad4159af20d5f14"
+  };
+ export default firebaseConfig;
